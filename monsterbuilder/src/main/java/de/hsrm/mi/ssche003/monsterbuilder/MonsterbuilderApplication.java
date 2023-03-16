798b88542766418583873b4e2345ddbf9015abdb
nussschleife <svenja_s98@freenet.de>
2023-03-16 08:35:38 +0100

    init

---
+++ monsterbuilder/src/main/java/de/hsrm/mi/ssche003/monsterbuilder/MonsterbuilderApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.ssche003.monsterbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterbuilderApplication.class, args);
+	}
+
+}
---
+++ monsterbuilder/src/test/java/de/hsrm/mi/ssche003/monsterbuilder/MonsterbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.ssche003.monsterbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
