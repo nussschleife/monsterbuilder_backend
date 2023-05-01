00de01cba6d61d0a54fbb59517111d518e19b246
nussschleife <svenja_s98@freenet.de>
2023-05-02 00:14:35 +0200

    update python skript with verhalten class

--- monsterbuilder/src/main/resources/skripts/MonsterTemplate.py
+++ monsterbuilder/src/main/resources/skripts/MonsterTemplate.py
@@ -1,4 +1,55 @@
-class Akteur:
+import random
+from de.hsrm.mi.ssche003.monsterbuilder.akteur.monster import Monster
+from de.hsrm.mi.ssche003.monsterbuilder.akteur.charakter import Charakter
 
-   def __init__(self):
-      a = 1;
+class States:
+   DEAD = 1
+   ALIVE = 2
+   WOUNDED = 3
+   SCARED = 4
+   PRONE = 5
+
+class AkteurVerhalten(object):
+    def initiative():
+       raise Exception('not implemented') 
+    
+    def reaktion():
+       raise Exception('not implemented') 
+       
+    def aktion():
+       raise Exception('not implemented') 
+      
+    def bewegen():
+       raise Exception('not implemented') 
+    
+class MonsterVerhalten(AkteurVerhalten):
+
+    state = States.ALIVE
+    monster = Monster()
+
+    def __init__(self, monster):
+        self.monster = monster
+
+    def initiative():
+       return int(random()*20)
+    
+    def reaktion(self):
+       a = 2
+       
+    def aktion(self):
+       if(self.state is States.WOUNDED):
+          return self.monster.getAlleAktionen()[0]
+
+    def bewegen(self):
+       r = 0
+
+class CharakterVerhalten(AkteurVerhalten):
+    charakter = Charakter()
+    state = States.ALIVE
+
+    def __init__(self, charakter):
+        self.charakter = charakter
+
+
+def berechneAktion():
+   return Aktion()
